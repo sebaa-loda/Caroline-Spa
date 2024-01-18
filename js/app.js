0309054f2462e7c7f1885d1be47bc72e0d75ea79
Sebaa <sebaamdq96@gmail.com>
2024-01-18 00:18:32 -0300

    end page

---
+++ js/app.js
@@ -0,0 +1,10 @@
+window.swiper = new Swiper({
+  el: '.slider__contenedor',
+  slideClass: 'slider__slide',
+  createElements: true,
+  autoplay: {
+    delay:5000
+  },
+  loop: true,
+  navigation: true,
+});
